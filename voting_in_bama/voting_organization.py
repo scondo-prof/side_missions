0c8d5758ae0787eb19721c2871d6bf836c3c4d5e
Scooter Condo <scondo.prof@gmail.com>
2025-09-06 00:05:54 -0400

    dev/fixed the add_fill_column_to_df function

--- voting_in_bama/voting_organization.py
+++ voting_in_bama/voting_organization.py
@@ -2,8 +2,11 @@
 import pandas as pd
 from pandas import DataFrame
 
-def create_data_frame_from_xlsx(xlsx_path: str) -> DataFrame:
-    df: DataFrame = pd.read_excel(xlsx_path, engine="openpyxl")
+def create_data_frame_from_xlsx(xlsx_path: str, sheet_name: str = None) -> DataFrame:
+    if sheet_name:
+        df: DataFrame = pd.read_excel(xlsx_path,sheet_name=sheet_name, engine="openpyxl")
+    else:
+        df: DataFrame = pd.read_excel(xlsx_path, engine="openpyxl")
 
     return df
 
@@ -11,17 +14,21 @@ def get_dataframe_columns(df: DataFrame) -> list[str]:
     return df.columns.to_list()
 
 
-def get_a6_pairs(df_columns: list[str], df: DataFrame) -> dict[str: list[str]]:
+def get_a6_pairs(df_columns: list[str], df: DataFrame) -> dict[str: list[int]]:
     if "a4" in df_columns and "a5" in df_columns and "a6" in df_columns:
         print("Starting to pair a6 value with a4_a5 key")
 
-        a6_pairs: dict[str: list[str]] = {}
+        a6_pairs: dict[str: list[int]] = {}
 
         for index, row in df.iterrows():
             
-            value_a4: str = row["a4"]
-            value_a5: str = row["a5"]
-            value_a6: str = row["a6"]
+            value_a4: str = str(row["a4"])
+            value_a5: str = str(row["a5"])
+            try:
+                value_a6: int = int(row["a6"])
+            except:
+                print(f"Value cant be made int: {row["a6"]}, making value 9999")
+                value_a6: int = 9999
 
             key: str = f"{value_a4} {value_a5}"
 
@@ -36,12 +43,27 @@ def get_a6_pairs(df_columns: list[str], df: DataFrame) -> dict[str: list[str]]:
     else:
         print("Required Columns are not Present")
 
+def add_fill_column_to_df(df: DataFrame, column_name: str) -> DataFrame:
+
+    df[column_name] = "fill"
+    return df
+
 
 
 
 
 if __name__ == "__main__":
-    df: DataFrame = create_data_frame_from_xlsx("test_Copy of RQ10719.xlsx")
-    df_columns: list[str] = get_dataframe_columns(df=df)
+    df = create_data_frame_from_xlsx(xlsx_path="test_Copy of RQ10719.xlsx")
+    df_columns = get_dataframe_columns(df=df)
 
-    print(get_a6_pairs(df_columns=df_columns, df=df))
+    a6_pairs = get_a6_pairs(df_columns=df_columns, df=df)
+
+    target_df = create_data_frame_from_xlsx(xlsx_path="test_Daphne District 5 Voter List 9.4.25.xlsx", sheet_name="Municipal Voters 8.26.25")
+    df_columns = get_dataframe_columns(df=target_df)
+
+    print(f"Pre Add: {df_columns}")
+
+    add_fill_column_to_df(df=target_df, column_name="a6")
+    df_columns = get_dataframe_columns(df=target_df)
+
+    print(f"Post Add: {df_columns}")
